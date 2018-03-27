9d050cf844db40d24b0eb29f0d8e21b13be9a0a2
Alberto Hernandez <albertohernandez@iMac-2.local>
2018-03-27 13:17:35 +0200

    search view

--- app/src/main/java/com/alberto/recipepuppy/ui/home/HomeActivity.java
+++ app/src/main/java/com/alberto/recipepuppy/ui/home/HomeActivity.java
@@ -1,15 +1,48 @@
 package com.alberto.recipepuppy.ui.home;
 
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.alberto.recipepuppy.R;
 import com.alberto.recipepuppy.common.view.activity.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends BaseActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        final MenuItem myActionMenuItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                myActionMenuItem.collapseActionView();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
     }
 }
